202c7cdf7ac26a1f3c761ebc87130009c7a1e24c
Shafi Rafiq <shafi_2008@live.com.au>
2020-05-26 23:25:57 +1000

    Done some error handling when posting device_code

--- cli/scripts/main.py
+++ cli/scripts/main.py
@@ -6,12 +6,30 @@ import os
 import dotenv
 import polling
 import time
+from box import Box
 
 dotenv.load_dotenv('/mnt/g/work/Axioms/sample-python-cli/.env')
 C_ID = os.getenv('CLIENT_ID')
 TENANT_DOMAIN = os.getenv('TENANT_DOMAIN')
 
 spinner = Halo(text='Loading', spinner='dots')
+
+# Errors/exceptions that are being used for the program
+class SampleError(Exception):
+    def __init__(self, message):
+        self.message = message 
+
+class ExpiredTokenError(SampleError):
+    '''Error raised when the expiry of the device code has been exceeded'''
+
+class AccessDeniedError(SampleError):
+    '''Error raised when the access has been denied by end user'''
+
+class SlowDownError(SampleError):
+    '''Error raised when poll interval needs to be increased by 5 seconds'''
+
+class AuthPendingError(SampleError):
+    '''Error raised when poll can continue after interval'''
 
 @click.group()
 def cli():
@@ -29,57 +47,68 @@ def login():
     """Asks user to login to Axioms account by opening Axiom login page"""
     # Begins login process
     click.echo('➡️  Starting Authorization')
-    # url = 'https://axioms.us.axioms.io/user/login'
-    # url = 'https://test-unlimited.uat.us.axioms.io/user/register' <------ TRY THISSSS
-    # the other one is not working apparently
-    # url = TENANT_DOMAIN + 'user/login'
-    # login_url = 'https://axioms.us.uat.axioms.io/user/login'
-    # webbrowser.open_new(login_url)
     
     # Beginning post request to device endpoint to obtain device_code, user_code, etc
     scope = 'openid profile email orgs roles permissions offline_response'
     client_data = {'client_id': C_ID, 'scope': scope}
     url = "https://{}/oauth2/device".format(TENANT_DOMAIN)
-    print(url)
     device_resp = requests.post(url, data=client_data, timeout=15, verify=False)
-    print(device_resp.json())
-    exchange_code_for_token(device_resp)
+    device_dict = Box(device_resp.json())
+    print(device_dict)
+    exchange_code_for_token(device_dict)
     # add try except to handle error
-
-# def get_auth_code():
-#     auth_code = input('➡️  Please enter authorization code on your screen: ').strip()
-#     return auth_code
 
 def  exchange_code_for_token(response):
     # Getting necessary variables 
     # use python box package
-    resp_dict = response.json()
-    device_code = resp_dict['device_code']
-    user_code = resp_dict['user_code']
-    verification_uri = resp_dict['verification_uri']
-    verification_uri_complete = resp_dict['verification_uri_complete']
-    interval = resp_dict['interval']
-    expire = resp_dict['expires_in']
+    # resp_dict = response.json()
+    device_code = response.device_code
+    user_code = response.user_code
+    verification_uri = response.verification_uri
+    verification_uri_complete = response.verification_uri_complete
+    interval = response.interval
+    expire = response.expires_in
     
     # Opening the browser (or providing url in the event browser does not open) to verification uri where user enters the user_code
     # use verifciation uri complete for convenience
-    click.echo('➡️  Please enter the given user code when prompted on website. If no webpage has been opened, please go to following link: {verification_uri}')
-    click.echo(f'User code: {user_code}')
-    
+    click.echo(f'➡️  Please follow the instructions on the following page: {verification_uri_complete}')
+    webbrowser.open_new(verification_uri_complete)  
     # Polling device code to token endpoint to get access token
     url = "https://{}/oauth2/token".format(TENANT_DOMAIN)
     req_data = {'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
                 'device_code': device_code, 
                 'client_id': C_ID}
-    webbrowser.open_new(verification_uri_complete)   
-    time.sleep(interval)
-    try:
-        token_response = polling.poll(lambda: requests.post(url, data=req_data),    
-                                    timeout=expire, 
-                                    step=interval,verify=False)
-    except polling.TimeoutException:
-        click.echo('Time out error. Please try again.') 
-    print(token_response.json())
+    time_out = time.time() + expire
+    time.sleep(3)
+    while True:
+        try:
+            token_resp = requests.post(url, data=req_data, verify=False)
+            token_dict = Box(token_resp.json())
+            if 'error' not in token_dict:
+                break
+            if token_dict.error == 'access_denied':
+                raise AccessDeniedError("Access has been denied.")
+            elif token_dict.error == 'expired_token':
+                raise ExpiredTokenError("Time has expired, please try again.")
+            elif token_dict.error == 'slow_down':
+                raise SlowDownError("Increasing interval time by 5 seconds.")
+            elif token_dict.error == 'authorization_pending':
+                raise AuthPendingError("Continuing polling after interval.")
+        except AccessDeniedError as e:
+            print(e.message)
+            break
+        except ExpiredTokenError as e:
+            print(e.message)
+            break
+        except SlowDownError as e:
+            print(e.message)
+            interval += 5
+        except AuthPendingError as e:
+            print(e.message)
+
+        time.sleep(interval)
+
+    print(token_resp.json())
 
 #def refresh 
 
@@ -87,9 +116,7 @@ def  exchange_code_for_token(response):
 def register():
     """Asks user to register to Axioms by opening Axiom registration page"""
     click.echo('➡️  Opening registration page')
-    #url = 'https://axioms.us.axioms.io/user/register'
-    #url = 'https://test-unlimited.uat.us.axioms.io/user/register' <------ TRY THISSSS
-    url = 'https://axioms.us.uat.axioms.io/user/register'
+    url = "https://{}/register".format(TENANT_DOMAIN)
     webbrowser.open_new(url)
     click.echo('➡️ After registration run ax login or axioms login')
 
@@ -97,8 +124,8 @@ cli.add_command(info)
 cli.add_command(login)
 cli.add_command(register)
 
-if __name__ == '__main__':
-    cli()
+# if __name__ == '__main__':
+#     cli()
 
 #######################################################################################
 # This should happen before client prompts user to login, check auth endpoint in devhub
@@ -113,3 +140,9 @@ if __name__ == '__main__':
 # device_data = {grant_type: , device_code: , client_id: }
 # token_resp = request.post('{tenant domain}/oauth2/token', data=device_data)
 # response from this will vary, check docs
+# try:
+    #     token_response = polling.poll(lambda: requests.post(url, data=req_data, verify=False),    
+    #                                 timeout=expire, 
+    #                                 step=interval)
+    # except polling.TimeoutException:
+    #     click.echo('Time out error. Please try again.') 
