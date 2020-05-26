3de6494f88be81036493444dd37985ac0428a8a5
Shafi Rafiq <shafi_2008@live.com.au>
2020-05-26 11:13:25 +1000

    Added more code to polling section, need to fix

--- cli/scripts/main.py
+++ cli/scripts/main.py
@@ -4,11 +4,12 @@ from halo import Halo
 import requests
 import os
 import dotenv
-#import polling
+import polling
+import time
 
 dotenv.load_dotenv('/mnt/g/work/Axioms/sample-python-cli/.env')
-c_id = os.getenv('CLIENT_ID')
-tenant_domain = os.getenv('TENANT_DOMAIN')
+C_ID = os.getenv('CLIENT_ID')
+TENANT_DOMAIN = os.getenv('TENANT_DOMAIN')
 
 spinner = Halo(text='Loading', spinner='dots')
 
@@ -31,46 +32,54 @@ def login():
     # url = 'https://axioms.us.axioms.io/user/login'
     # url = 'https://test-unlimited.uat.us.axioms.io/user/register' <------ TRY THISSSS
     # the other one is not working apparently
-    # url = tenant_domain + 'user/login'
-    login_url = 'https://axioms.us.uat.axioms.io/user/login'
-    webbrowser.open_new(login_url)
+    # url = TENANT_DOMAIN + 'user/login'
+    # login_url = 'https://axioms.us.uat.axioms.io/user/login'
+    # webbrowser.open_new(login_url)
     
     # Beginning post request to device endpoint to obtain device_code, user_code, etc
     scope = 'openid profile email orgs roles permissions offline_response'
-    client_data = {'client_id': c_id, 'scope': scope}
-    url = tenant_domain + 'oauth2/device'
-    device_resp = requests.post(url, data=client_data, timeout=15)
+    client_data = {'client_id': C_ID, 'scope': scope}
+    url = "https://{}/oauth2/device".format(TENANT_DOMAIN)
+    print(url)
+    device_resp = requests.post(url, data=client_data, timeout=15, verify=False)
     print(device_resp.json())
-    auth_code = get_auth_code()
+    exchange_code_for_token(device_resp)
+    # add try except to handle error
 
-def get_auth_code():
-    auth_code = input('➡️  Please enter authorization code on your screen: ').strip()
-    return auth_code
+# def get_auth_code():
+#     auth_code = input('➡️  Please enter authorization code on your screen: ').strip()
+#     return auth_code
 
 def  exchange_code_for_token(response):
     # Getting necessary variables 
+    # use python box package
     resp_dict = response.json()
     device_code = resp_dict['device_code']
     user_code = resp_dict['user_code']
     verification_uri = resp_dict['verification_uri']
+    verification_uri_complete = resp_dict['verification_uri_complete']
     interval = resp_dict['interval']
     expire = resp_dict['expires_in']
     
     # Opening the browser (or providing url in the event browser does not open) to verification uri where user enters the user_code
+    # use verifciation uri complete for convenience
     click.echo('➡️  Please enter the given user code when prompted on website. If no webpage has been opened, please go to following link: {verification_uri}')
     click.echo(f'User code: {user_code}')
     
     # Polling device code to token endpoint to get access token
-    url = tenant + 'oauth2/token'
+    url = "https://{}/oauth2/token".format(TENANT_DOMAIN)
     req_data = {'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
                 'device_code': device_code, 
-                'client_id': c_id}
+                'client_id': C_ID}
+    webbrowser.open_new(verification_uri_complete)   
+    time.sleep(interval)
     try:
         token_response = polling.poll(lambda: requests.post(url, data=req_data),    
                                     timeout=expire, 
-                                    step=interval)
+                                    step=interval,verify=False)
     except polling.TimeoutException:
         click.echo('Time out error. Please try again.') 
+    print(token_response.json())
 
 #def refresh 
 
